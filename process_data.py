7067ca73a94617c9f30e6385bd692081a7e868b9
Junyu Wei <Junyu Wei>
2015-12-03 03:07:11 +0000

    Added some logs for 2-point taps

--- process_data.py
+++ process_data.py
@@ -1,4 +1,4 @@
-file = open('2_points/left_hand/gyro/2', 'r+', encoding='utf-8')
+file = open('logs/2_points/left_hand/gyro/1', 'r+', encoding='utf-8')
 content = file.read()
 print(content)
 file.close()
