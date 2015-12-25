277b7175627e2f293e85de8de44071e7c4369567
Junyu Wei <junyuwei@gmail.com>
2015-12-26 00:55:08 +0800

    Processes data for left hand, 2 points, lin acc.

--- process_data.py
+++ process_data.py
@@ -1,31 +1,44 @@
-def process_left_hand():
-    file = open('logs/2_points/left_hand/lin_acc/5', 'r+', encoding='utf-8')
-    content = file.read()
-    file.close()
+import json
+import os
 
-    split_lines = content.splitlines()
 
-    left_taps = []
-    right_taps = []
+def process_2p_left_hand_lin_acc():
+    log_dir = os.path.join('logs', '2_points', 'left_hand', 'lin_acc')
+    file_names = os.listdir(log_dir)  # list of strings, '1', '2', etc
+    for file_name in file_names:
+        file = open(os.path.join(log_dir, file_name), 'r+', encoding='utf-8')
+        content = file.read()
+        file.close()
 
-    for i in range(len(split_lines)):
-        left_tap_index = 0
-        right_tap_index = 0
-        if i > 1:  # Start at the second line
-            split_line = split_lines[i].split(",")
-            split_line_prev = split_lines[i-1].split(",")
-            if int(split_line[1]) == 1 and int(split_line_prev[1]) == 1:
-                left_taps[left_tap_index].append(split_lines[i])
-            elif int(split_line[1]) == 1 and int(split_line_prev[1]) == 2:
-                left_taps[left_tap_index].append(split_lines[i])
-            elif int(split_line[1]) == 2 and int(split_line_prev[1]) == 1:
-                left_tap_index += 1
-                right_taps[right_tap_index].append(split_lines[i])
-            elif int(split_line[1]) == 2 and int(split_line_prev[1]) == 2:
-                right_taps[right_tap_index].append(split_lines[i])
+        split_lines = content.splitlines()
 
-        print(left_taps)
+        left_taps = []
+        right_taps = []
+        data_window = []
 
+        for i in range(len(split_lines)):
+            if i > 0:  # Start at the second line
+                split_line = split_lines[i].split(",")
+                split_line_prev = split_lines[i - 1].split(",")
+                if int(split_line[1]) == int(split_line_prev[1]):
+                    data_window.append(split_lines[i])
+                elif int(split_line[1]) == 2 and int(split_line_prev[1]) == 1:
+                    left_taps.append(data_window.copy())
+                    data_window.clear()
+                    data_window.append(split_lines[i])
+                elif int(split_line[1]) == 1 and int(split_line_prev[1]) == 2:
+                    right_taps.append(data_window.copy())
+                    data_window.clear()
+                    data_window.append(split_lines[i])
+                if i == (len(split_lines) - 1):  # Last line
+                    right_taps.append(data_window.copy())
+
+        print(json.dumps(left_taps, indent=2))
+        print(json.dumps(right_taps, indent=2))
+        # print(right_taps)
+
+
+process_2p_left_hand_lin_acc()
 
 # for i in range(len(split_lines)):
 #     if i < 10:
@@ -33,8 +46,8 @@ def process_left_hand():
 
 # for line in split_lines:
 #     print(line)
-    # split_line = line.split(",")
-    # print(split_line)
+# split_line = line.split(",")
+# print(split_line)
 
 
 # file = open('data', 'r+', encoding='utf-8')
---
+++ test_scripts.py
@@ -0,0 +1,5 @@
+abc = [1, 2, 3, 4, 5]
+print(abc)
+
+abc.clear()
+print(abc)
