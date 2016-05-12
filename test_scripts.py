6e29b3678a61816c98a49a9e236a0085e39a4cd9
Junyu Wei <junyuwei@gmail.com>
2016-05-12 11:17:25 +0100

    Using actual sensor values in testing feature calculations.

--- test_scripts.py
+++ test_scripts.py
@@ -3,15 +3,15 @@ from make_training_file import get_sample_kurtosis
 from make_training_file import get_sample_skew
 
 lin_acc_sample = [[
-    "2,9,3,55,12",
-    "1,-3,4,14,33",
-    "-4,16,22,12,8"
+    "1454432615403, 1, -0.5762239694595337, 0.16243454813957214,0.0730307474732399",
+    "1454432615423, 1, -0.5722370743751526, 0.15929098427295685,0.049130234867334366",
+    "1454432615445, 1, -0.5722819566726685, 0.16261398792266846,0.3850666582584381",
 ]]
 
 gyro_sample = [[
-    "3,0,-1,8,55",
-    ",0,0,18,4",
-    "0,0,2,66,8"
+    "1454432615398, 1, 0.021978000178933144, 0.23076899349689484,-0.41391900181770325",
+    "1454432615419, 1, 0.020756999030709267, 0.27838799357414246,-0.40903499722480774",
+    "1454432615439, 1, 0.07203900068998337, 0.3638579845428467, -0.40659299492836"
 ]]
 
 # Things to test for tap detection:
@@ -24,13 +24,8 @@ gyro_sample = [[
 # 7. frobenius norm
 # 8. pearson coefficients
 
-
-
 print(get_sample_mean(lin_acc_sample))
-print(get_sample_mean(gyro_sample))
-
 print(get_sample_skew(lin_acc_sample))
-print(get_sample_skew(gyro_sample))
 
 # import os
 #
